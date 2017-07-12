71c023f394e53d6e4d01114700fc6b0dc43db2e2
Paul Done <pkdone@gmail.com>
2017-07-12 12:14:55 +0100

    Script improvements for looping to create/delete disks & volumes

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -2,63 +2,55 @@
 ##
 # Script to deploy a Kubernetes project with a StatefulSet running a MongoDB Sharded Cluster, to GKE.
 ##
+
 NEW_PASSWORD="abc123"
+
 
 # Create new GKE Kubernetes cluster (using host node VM images based on Debian
 # rather than ChromiumOS default & also use slightly larger VMs than default)
 echo "Creating GKE Cluster"
 gcloud container clusters create "gke-mongodb-demo-cluster" --image-type=CONTAINER_VM --machine-type=n1-standard-2
 
+
 # Configure host VM using daemonset to add XFS mounting support and disable hugepages
 echo "Deploying GKE Daemon Set"
 kubectl apply -f ../resources/hostvm-node-configurer-daemonset.yaml
+
 
 # Register GCE Fast SSD persistent disks and then create the persistent disks 
 echo "Creating GCE disks"
 kubectl apply -f ../resources/gce-ssd-storageclass.yaml
 sleep 5
-gcloud compute disks create --size 4GB --type pd-ssd pd-ssd-disk-4g-1
-gcloud compute disks create --size 4GB --type pd-ssd pd-ssd-disk-4g-2
-gcloud compute disks create --size 4GB --type pd-ssd pd-ssd-disk-4g-3
-gcloud compute disks create --size 8GB --type pd-ssd pd-ssd-disk-8g-1
-gcloud compute disks create --size 8GB --type pd-ssd pd-ssd-disk-8g-2
-gcloud compute disks create --size 8GB --type pd-ssd pd-ssd-disk-8g-3
-gcloud compute disks create --size 8GB --type pd-ssd pd-ssd-disk-8g-4
-gcloud compute disks create --size 8GB --type pd-ssd pd-ssd-disk-8g-5
-gcloud compute disks create --size 8GB --type pd-ssd pd-ssd-disk-8g-6
-gcloud compute disks create --size 8GB --type pd-ssd pd-ssd-disk-8g-7
-gcloud compute disks create --size 8GB --type pd-ssd pd-ssd-disk-8g-8
-gcloud compute disks create --size 8GB --type pd-ssd pd-ssd-disk-8g-9
+for i in 1 2 3
+do
+    # 4GB disks    
+    gcloud compute disks create --size 4GB --type pd-ssd pd-ssd-disk-4g-$i
+done
+for i in 1 2 3 4 5 6 7 8 9
+do
+    # 8 GB disks
+    gcloud compute disks create --size 8GB --type pd-ssd pd-ssd-disk-8g-$i
+done
 sleep 3
 
+
 # Create persistent volumes using disks created above
-echo "Creating GKE Peristent Volumes"
-sed -e 's/INST/1/g; s/SIZE/4/g' ../resources/xfs-gce-ssd-persistentvolume.yaml > /tmp/xfs-gce-ssd-persistentvolume.yaml
-kubectl apply -f /tmp/xfs-gce-ssd-persistentvolume.yaml
-sed -e 's/INST/2/g; s/SIZE/4/g' ../resources/xfs-gce-ssd-persistentvolume.yaml > /tmp/xfs-gce-ssd-persistentvolume.yaml
-kubectl apply -f /tmp/xfs-gce-ssd-persistentvolume.yaml
-sed -e 's/INST/3/g; s/SIZE/4/g' ../resources/xfs-gce-ssd-persistentvolume.yaml > /tmp/xfs-gce-ssd-persistentvolume.yaml
-kubectl apply -f /tmp/xfs-gce-ssd-persistentvolume.yaml
-sed -e 's/INST/1/g; s/SIZE/8/g' ../resources/xfs-gce-ssd-persistentvolume.yaml > /tmp/xfs-gce-ssd-persistentvolume.yaml
-kubectl apply -f /tmp/xfs-gce-ssd-persistentvolume.yaml
-sed -e 's/INST/2/g; s/SIZE/8/g' ../resources/xfs-gce-ssd-persistentvolume.yaml > /tmp/xfs-gce-ssd-persistentvolume.yaml
-kubectl apply -f /tmp/xfs-gce-ssd-persistentvolume.yaml
-sed -e 's/INST/3/g; s/SIZE/8/g' ../resources/xfs-gce-ssd-persistentvolume.yaml > /tmp/xfs-gce-ssd-persistentvolume.yaml
-kubectl apply -f /tmp/xfs-gce-ssd-persistentvolume.yaml
-sed -e 's/INST/4/g; s/SIZE/8/g' ../resources/xfs-gce-ssd-persistentvolume.yaml > /tmp/xfs-gce-ssd-persistentvolume.yaml
-kubectl apply -f /tmp/xfs-gce-ssd-persistentvolume.yaml
-sed -e 's/INST/5/g; s/SIZE/8/g' ../resources/xfs-gce-ssd-persistentvolume.yaml > /tmp/xfs-gce-ssd-persistentvolume.yaml
-kubectl apply -f /tmp/xfs-gce-ssd-persistentvolume.yaml
-sed -e 's/INST/6/g; s/SIZE/8/g' ../resources/xfs-gce-ssd-persistentvolume.yaml > /tmp/xfs-gce-ssd-persistentvolume.yaml
-kubectl apply -f /tmp/xfs-gce-ssd-persistentvolume.yaml
-sed -e 's/INST/7/g; s/SIZE/8/g' ../resources/xfs-gce-ssd-persistentvolume.yaml > /tmp/xfs-gce-ssd-persistentvolume.yaml
-kubectl apply -f /tmp/xfs-gce-ssd-persistentvolume.yaml
-sed -e 's/INST/8/g; s/SIZE/8/g' ../resources/xfs-gce-ssd-persistentvolume.yaml > /tmp/xfs-gce-ssd-persistentvolume.yaml
-kubectl apply -f /tmp/xfs-gce-ssd-persistentvolume.yaml
-sed -e 's/INST/9/g; s/SIZE/8/g' ../resources/xfs-gce-ssd-persistentvolume.yaml > /tmp/xfs-gce-ssd-persistentvolume.yaml
-kubectl apply -f /tmp/xfs-gce-ssd-persistentvolume.yaml
+echo "Creating GKE Persistent Volumes"
+for i in 1 2 3
+do
+    # Replace text stating volume number + size of disk (set to 4)
+    sed -e "s/INST/${i}/g; s/SIZE/4/g" ../resources/xfs-gce-ssd-persistentvolume.yaml > /tmp/xfs-gce-ssd-persistentvolume.yaml
+    kubectl apply -f /tmp/xfs-gce-ssd-persistentvolume.yaml
+done
+for i in 1 2 3 4 5 6 7 8 9
+do
+    # Replace text stating volume number + size of disk (set to 8)
+    sed -e "s/INST/${i}/g; s/SIZE/8/g" ../resources/xfs-gce-ssd-persistentvolume.yaml > /tmp/xfs-gce-ssd-persistentvolume.yaml
+    kubectl apply -f /tmp/xfs-gce-ssd-persistentvolume.yaml
+done
 rm /tmp/xfs-gce-ssd-persistentvolume.yaml
 sleep 3
+
 
 # Create keyfile for the MongoDB cluster as a Kubernetes shared secret
 TMPFILE=$(mktemp)
@@ -66,9 +58,11 @@ TMPFILE=$(mktemp)
 kubectl create secret generic shared-bootstrap-data --from-file=internal-auth-mongodb-keyfile=$TMPFILE
 rm $TMPFILE
 
+
 # Deploy a MongoDB ConfigDB Service ("Config Server Replica Set") using a StatefulSet
 echo "Deploying GKE StatefulSet & Service for MongoDB Config Server Replica Set"
 kubectl apply -f ../resources/mongodb-configdb-service.yaml
+
 
 # Deploy each MongoDB Shard Service using a StatefulSet
 echo "Deploying GKE StatefulSet & Service for each MongoDB Shard Replica Set"
@@ -80,7 +74,8 @@ sed -e 's/shardX/shard3/g; s/ShardX/Shard3/g' ../resources/mongodb-maindb-servic
 kubectl apply -f /tmp/mongodb-maindb-service.yaml
 rm /tmp/mongodb-maindb-service.yaml
 
-# Intialise the Config Server Replica Set and each Shard Replica Set
+
+# Initialise the Config Server Replica Set and each Shard Replica Set
 echo "Sleeping for 90 seconds to allow all the Stateful Sets' Pods to come up"
 sleep 90
 echo "Configuring Config Server Replica Set"
@@ -92,22 +87,27 @@ kubectl exec mongod-shard3-0 -c mongod-shard3-container -- mongo --eval 'rs.init
 echo "Waiting for all the replica sets to initialise..."
 sleep 15
 
+
 # Deploy some Mongos Routers using a Deployment
 echo "Deploying GKE Deployment & Service for some Mongos Routers"
 kubectl apply -f ../resources/mongodb-mongos-deployment.yaml
 
-# Add shards to the configdb
+
+# Add Shards to the Configdb
 echo "Sleeping for 20 seconds to allow all the Mongos routers to come up"
 sleep 20
+echo "Configuring ConfigDB to be aware of the 3 Shards"
 kubectl exec $(kubectl get pod -l "tier=routers" -o jsonpath='{.items[0].metadata.name}') -c mongos-container -- mongo --eval 'sh.addShard("Shard1RepSet/mongod-shard1-0.mongodb-shard1-service.default.svc.cluster.local:27017");'
 kubectl exec $(kubectl get pod -l "tier=routers" -o jsonpath='{.items[0].metadata.name}') -c mongos-container -- mongo --eval 'sh.addShard("Shard2RepSet/mongod-shard2-0.mongodb-shard2-service.default.svc.cluster.local:27017");'
 kubectl exec $(kubectl get pod -l "tier=routers" -o jsonpath='{.items[0].metadata.name}') -c mongos-container -- mongo --eval 'sh.addShard("Shard3RepSet/mongod-shard3-0.mongodb-shard3-service.default.svc.cluster.local:27017");'
 
-# Create the admin user (this will automatically disable the localhost exception)
+
+# Create the Admin User (this will automatically disable the localhost exception)
 echo "Creating user: 'main_admin'"
 kubectl exec $(kubectl get pod -l "tier=routers" -o jsonpath='{.items[0].metadata.name}') -c mongos-container -- mongo --eval 'db.getSiblingDB("admin").createUser({user:"main_admin",pwd:"'"${NEW_PASSWORD}"'",roles:[{role:"root",db:"admin"}]});'
 
-# Print current deployment state 
+
+# Print Summary State of Deployment
 echo
 kubectl get persistentvolumes
 kubectl get all 
--- scripts/teardown.sh
+++ scripts/teardown.sh
@@ -3,7 +3,7 @@
 # Script to remove/undepoy all project resources from GKE & GCE.
 ##
 
-# Delete mongod stateful set + mongodb service + secrets + host vm configuer daemonset
+# Delete mongos deployment + mongod stateful set + mongodb service + secrets + host vm configurer daemonset
 kubectl delete deployments mongos
 kubectl delete statefulsets mongod-shard1
 kubectl delete services mongodb-shard1-service
@@ -23,33 +23,25 @@ kubectl delete persistentvolumeclaims -l tier=configdb
 sleep 3
 
 # Delete persistent volumes
-kubectl delete persistentvolumes data-volume-4g-1
-kubectl delete persistentvolumes data-volume-4g-2
-kubectl delete persistentvolumes data-volume-4g-3
-kubectl delete persistentvolumes data-volume-8g-1
-kubectl delete persistentvolumes data-volume-8g-2
-kubectl delete persistentvolumes data-volume-8g-3
-kubectl delete persistentvolumes data-volume-8g-4
-kubectl delete persistentvolumes data-volume-8g-5
-kubectl delete persistentvolumes data-volume-8g-6
-kubectl delete persistentvolumes data-volume-8g-7
-kubectl delete persistentvolumes data-volume-8g-8
-kubectl delete persistentvolumes data-volume-8g-9
+for i in 1 2 3
+do
+    kubectl delete persistentvolumes data-volume-4g-$i
+done
+for i in 1 2 3 4 5 6 7 8 9
+do
+    kubectl delete persistentvolumes data-volume-8g-$i
+done
 sleep 20
 
 # Delete GCE disks
-gcloud -q compute disks delete pd-ssd-disk-4g-1
-gcloud -q compute disks delete pd-ssd-disk-4g-2
-gcloud -q compute disks delete pd-ssd-disk-4g-3
-gcloud -q compute disks delete pd-ssd-disk-8g-1
-gcloud -q compute disks delete pd-ssd-disk-8g-2
-gcloud -q compute disks delete pd-ssd-disk-8g-3
-gcloud -q compute disks delete pd-ssd-disk-8g-4
-gcloud -q compute disks delete pd-ssd-disk-8g-5
-gcloud -q compute disks delete pd-ssd-disk-8g-6
-gcloud -q compute disks delete pd-ssd-disk-8g-7
-gcloud -q compute disks delete pd-ssd-disk-8g-8
-gcloud -q compute disks delete pd-ssd-disk-8g-9
+for i in 1 2 3
+do
+    gcloud -q compute disks delete pd-ssd-disk-4g-$i
+done
+for i in 1 2 3 4 5 6 7 8 9
+do
+    gcloud -q compute disks delete pd-ssd-disk-8g-$i
+done
 
 # Delete whole Kubernetes cluster (including its VM instances)
 gcloud -q container clusters delete "gke-mongodb-demo-cluster"
